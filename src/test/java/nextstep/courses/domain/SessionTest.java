ff4179f4be011c0e86abf285bf0f420a91bb5214
모재영 <mo@utransfer.com>
2025-12-09 01:21:25 +0900

    test : 깨진 테스트 고치기

--- src/test/java/nextstep/courses/domain/SessionTest.java
+++ src/test/java/nextstep/courses/domain/SessionTest.java
@@ -3,8 +3,10 @@ package nextstep.courses.domain;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import nextstep.courses.enumerate.CoverImageType;
 import nextstep.courses.enumerate.ProvideType;
+import nextstep.courses.enumerate.SessionStatusType;
 import org.junit.jupiter.api.Test;
 
 class SessionTest {
@@ -15,18 +17,29 @@ class SessionTest {
     static {
         try {
             freeSession = new Session(
+                1L,
                 "1",
                 new SessionBody("title", "content"),
                 new Duration(LocalDate.now().plusDays(1), LocalDate.now().plusDays(3)),
                 new CoverImage(1_500_000, CoverImageType.JPEG, 300, 200),
-                new Provide(ProvideType.FREE)
+                new SessionStatus(SessionStatusType.RECRUITING),
+                new Provide(ProvideType.FREE),
+                5,
+                LocalDateTime.now(),
+                null
             );
+            
             paidSession = new Session(
+                2L,
                 "1",
                 new SessionBody("title", "content"),
                 new Duration(LocalDate.now().plusDays(1), LocalDate.now().plusDays(3)),
                 new CoverImage(1_500_000, CoverImageType.JPEG, 300, 200),
-                new Provide(ProvideType.PAID, new ProvidePolicy(10, 10))
+                new SessionStatus(SessionStatusType.RECRUITING),
+                new Provide(ProvideType.PAID, new ProvidePolicy(10, 10)),
+                5,
+                LocalDateTime.now(),
+                null
             );
         } catch(Exception e) {
             throw new RuntimeException(e);
@@ -36,24 +49,28 @@ class SessionTest {
     
     @Test
     void 무료_session을_생성한다() throws Exception {
-        new Session(
-            "1",
-            new SessionBody("title", "content"),
-            new Duration(LocalDate.now().plusDays(1), LocalDate.now().plusDays(3)),
-            new CoverImage(1_500_000, CoverImageType.JPEG, 300, 200),
-            new Provide(ProvideType.FREE)
-        );
+        assertThatNoException().isThrownBy(() -> {
+            new Session(
+                "1",
+                new SessionBody("title", "content"),
+                new Duration(LocalDate.now().plusDays(1), LocalDate.now().plusDays(3)),
+                new CoverImage(1_500_000, CoverImageType.JPEG, 300, 200),
+                new Provide(ProvideType.FREE)
+            );
+        });
     }
     
     @Test
     void 유료_session을_생성한다() throws Exception {
-        new Session(
-            "1",
-            new SessionBody("title", "content"),
-            new Duration(LocalDate.now().plusDays(1), LocalDate.now().plusDays(3)),
-            new CoverImage(1_500_000, CoverImageType.JPEG, 300, 200),
-            new Provide(ProvideType.PAID, new ProvidePolicy(10, 10))
-        );
+        assertThatNoException().isThrownBy(() -> {
+            new Session(
+                "1",
+                new SessionBody("title", "content"),
+                new Duration(LocalDate.now().plusDays(1), LocalDate.now().plusDays(3)),
+                new CoverImage(1_500_000, CoverImageType.JPEG, 300, 200),
+                new Provide(ProvideType.PAID, new ProvidePolicy(10, 10))
+            );
+        });
     }
     
     @Test
